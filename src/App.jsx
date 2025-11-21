b0d597500d86a5ff6a4fb259153ebc0b828a9a29
AntonellaSimonetti <antonella.simonetti98@gmail.com>
2025-11-21 00:13:25 -0300

    feat: add header component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/header/header.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <h1 className="logo">
+          Neo<span>Tech</span> <span className="accent">Accessories</span>
+        </h1>
+      </div>
+    </header>
+  );
+}
