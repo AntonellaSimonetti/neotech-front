8d5dba92411d8af494313b08e24fefa4af215dcb
clarita <claraelizabethfarias@gmail.com>
2025-11-21 21:37:34 -0300

    cambio en logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     <header className="header">
       <div className="logo-container">
         <h1 className="logo">
-          Neo<span>Tech</span> <span className="accent">Accessories</span>
+          Neo<span>Tech</span> <span className="accent">Tecnología</span>
         </h1>
       </div>
       <NavBar />
