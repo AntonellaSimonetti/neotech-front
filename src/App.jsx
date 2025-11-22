634744e6c9807ddef19f5f82b2d0b6aed9fd85a7
clarita <claraelizabethfarias@gmail.com>
2025-11-22 18:11:07 -0300

    add mision y vision + estilos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer/Footer.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Contact from "./pages/Contact";
+import MisionVision from "./pages/MisionVision";
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,9 +12,6 @@ export default function App() {
   return (
     <Router>
       <Header />
-      
-
-
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/productos" element={<h1>Productos</h1>} />
@@ -21,6 +19,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/misionvision" element={<MisionVision />} />
 
       </Routes>
       <Footer />
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";  
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 — Página hecha por <span>Antonella Simonetti & Clara Farias</span></p>
+      <p>© 2025 — Página hecha por <Link to="/misionvision"><span>Antonella Simonetti & Clara Farias</span></Link></p>
     </footer>
   );
 };
