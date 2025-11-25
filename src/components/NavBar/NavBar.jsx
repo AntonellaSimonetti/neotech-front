868128a9acd7c894bb01a162dc0748d641eb1912
clarita <claraelizabethfarias@gmail.com>
2025-11-24 22:35:33 -0300

    fix NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { LogIn, User, LogOut, ShoppingCart } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuthStatus } from "../../hooks/useAuthStatus";
 import "./NavBar.css";
@@ -8,6 +9,9 @@ export default function NavBar() {
   const navigate = useNavigate();
 
   const [open, setOpen] = useState(false);
+
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -26,44 +30,105 @@ export default function NavBar() {
 
       <nav className="nav-bar">
         <ul className={`nav-menu ${open ? "active" : ""}`}>
-
           {isAdmin ? (
             <>
-              <li><Link className="nav-item" to="/admin" onClick={closeMenu}>Dashboard</Link></li>
-              <li><Link className="nav-item" to="/admin/productos" onClick={closeMenu}>Productos</Link></li>
-              <li><Link className="nav-item" to="/admin/ordenes" onClick={closeMenu}>Órdenes</Link></li>
+              <li>
+                <Link className="nav-item" to="/admin" onClick={closeMenu}>
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="nav-item"
+                  to="/admin/productos"
+                  onClick={closeMenu}
+                >
+                  Productos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="nav-item"
+                  to="/admin/ordenes"
+                  onClick={closeMenu}
+                >
+                  Órdenes
+                </Link>
+              </li>
 
               <li
-                className="nav-item"
+                className="btn-login"
                 onClick={() => {
                   closeMenu();
                   handleLogout();
                 }}
               >
-                Salir
+                <span>{storedUser?.nombre || "Dueño"}</span>
+                <LogOut size={18} />
               </li>
             </>
           ) : (
             <>
-              <li><Link className="nav-item" to="/" onClick={closeMenu}>Inicio</Link></li>
-              <li><Link className="nav-item" to="/accesorios" onClick={closeMenu}>Accesorios</Link></li>
-              <li><Link className="nav-item" to="/computadoras" onClick={closeMenu}>Computadoras</Link></li>
-              <li><Link className="nav-item" to="/contacto" onClick={closeMenu}>Contacto</Link></li>
+              <li>
+                <Link className="nav-item" to="/" onClick={closeMenu}>
+                  Inicio
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-item" to="/accesorios" onClick={closeMenu}>
+                  Accesorios
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="nav-item"
+                  to="/computadoras"
+                  onClick={closeMenu}
+                >
+                  Computadoras
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-item" to="/contacto" onClick={closeMenu}>
+                  Contacto
+                </Link>
+              </li>
 
               {isLogged ? (
                 <>
-                  <li><Link className="nav-item" to="/perfil" onClick={closeMenu}>Perfil</Link></li>
-                  <li><Link className="nav-item" to="/mis-compras" onClick={closeMenu}>Mis Compras</Link></li>
-                  <li><Link className="nav-item" to="/favoritos" onClick={closeMenu}>Favoritos</Link></li>
+                  <li>
+                    <Link className="nav-item" to="/perfil" onClick={closeMenu}>
+                      Perfil
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="nav-item"
+                      to="/mis-compras"
+                      onClick={closeMenu}
+                    >
+                      Mis Compras
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="nav-item"
+                      to="/favoritos"
+                      onClick={closeMenu}
+                    >
+                      Favoritos
+                    </Link>
+                  </li>
 
                   <li
-                    className="nav-item"
+                    className="btn-login"
                     onClick={() => {
                       closeMenu();
                       handleLogout();
                     }}
                   >
-                    Salir
+                    <span>{storedUser?.nombre || "Cliente"}</span>
+                    <LogOut size={18} />
                   </li>
 
                   <li className="cart-wrapper">
@@ -74,8 +139,12 @@ export default function NavBar() {
                 </>
               ) : (
                 <>
-                  <li><Link className="nav-item" to="/login" onClick={closeMenu}>Iniciar sesión</Link></li>
-
+                  <li>
+                    <Link className="btn-login" to="/login" onClick={closeMenu}>
+                      <LogIn size={18} />
+                      Iniciar sesión
+                    </Link>
+                  </li>
                 </>
               )}
             </>
