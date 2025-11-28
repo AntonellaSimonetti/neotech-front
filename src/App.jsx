d739538345f083ded00a748a95f51a06647a2d10
AntonellaSimonetti <153952535+AntonellaSimonetti@users.noreply.github.com>
2025-11-28 10:07:05 -0300

    fix style app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header.jsx";
+import Header from "./components/header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Register from "./pages/Register/Register.jsx";
